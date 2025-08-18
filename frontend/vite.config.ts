87aaafb9d2580a39e9c3627b3734f67ea0e10775
Jonathan St-Onge <jonathanstonge7@gmail.com>
2025-08-18 09:12:49 -0400

    working map

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dsv from "@rollup/plugin-dsv";
+import path from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), dsv()],
+	resolve: {
+		alias: {
+			$data: path.resolve("./src/data"),
+			$styles: path.resolve("./src/styles"),
+		}
+	},
 });
